98011797f5553682cd97d4c948ea8867383821f8
Austin Paley <austinpaley32@gmail.com>
2018-02-26 17:45:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object
+  object[key]
   
   return Object
   
