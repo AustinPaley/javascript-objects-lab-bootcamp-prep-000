ee68174b28cbd5ddf94fd17c3af3f03f38bbba33
Austin Paley <austinpaley32@gmail.com>
2018-02-26 18:08:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key){
   var newobject = Object.assign({}, object);
-  delete new[key];
+  delete newobject[key];
   return new
 }
 
